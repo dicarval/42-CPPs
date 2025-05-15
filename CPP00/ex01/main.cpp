e0801703b0a12645bd5040e2645b68135b0f4b9f
Diogo Alexandre Carvalho Santos <dicarval@c1r10s5.42lisboa.com>
2025-05-15 14:20:39 +0100

    GO

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dicarval <dicarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:51:25 by dicarval          #+#    #+#             */
-/*   Updated: 2025/05/15 14:15:45 by dicarval         ###   ########.fr       */
+/*   Updated: 2025/05/15 14:19:56 by dicarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,10 @@ int	get_command(PhoneBook *Pb)
 			Pb->search();
 		else if (input == "EXIT")
 			return (SUCCESS);
+		else if (std::cin)
+			std::cout << "Not a valid command, try again" << std::endl;
 		if (!std::cin)
 			return (UNSUCCESS);
-		else
-			std::cout << "Not a valid command, try again" << std::endl;
 		system("sleep 1");
 		system("clear");
 		std::cout << "What's next?" << std::endl;
