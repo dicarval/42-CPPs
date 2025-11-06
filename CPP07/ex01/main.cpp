306d065bca88cd9d10ee3ef068e7e97885f849d8
Diogo Alexandre Carvalho Santos <dicarval@c2r7s12.42lisboa.com>
2025-11-06 13:24:59 +0000

    Ready to deliver the project

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dicarval <dicarval@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 13:30:30 by dicarval          #+#    #+#             */
-/*   Updated: 2025/11/06 10:19:43 by dicarval         ###   ########.fr       */
+/*   Updated: 2025/11/06 13:24:36 by dicarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main()
 	char				*brah = const_cast<char*>(abacaxi);
 	int					array[] = {3,4,5,6,7,8,9};
 	const int			arraaay[] = {333,444,555,666,777,888,999};
+	float				f[] = {42.42, 42.42, 42.42, 42.42, 42.42};
 
 	iter(abacaxi, 7, printIt);
 	//iter(abacaxi, 7, changeIt);
@@ -29,4 +30,5 @@ int	main()
 	iter(array, 7, changeIt);
 	iter(arraaay, 7, printIt);
 	//iter(arraaay, 7, changeIt);
+	iter(f, 5, printIt);
 }
